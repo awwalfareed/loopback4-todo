e4d3abf6cd0d414b1f88207400e5e3b74b1f2787
Muhammad Fareed <mohammad.fareed@binaryinformatics.com>
2022-10-17 23:26:45 +0530

    first commit

---
+++ src/models/todo.model.ts
@@ -0,0 +1,41 @@
+import {Entity, model, property} from '@loopback/repository';
+import {not} from 'should';
+
+@model()
+export class Todo extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  title: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  desc: string;
+
+  @property({
+    type: 'string',
+    required: not,
+  })
+  isComplete: string;
+
+
+  constructor(data?: Partial<Todo>) {
+    super(data);
+  }
+}
+
+export interface TodoRelations {
+  // describe navigational properties here
+}
+
+export type TodoWithRelations = Todo & TodoRelations;
